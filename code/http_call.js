5267dec583e75bfe3f8aaa57e86bbce6ca6a6785
hyeonseo-chae <eric71804122@gmail.com>
2019-11-12 20:25:33 +0900

    중앙대학교 빅스비 세미나 캡슐 탬플릿

--- code/http_call.js
+++ code/http_call.js
@@ -1,5 +1,5 @@
-module.exports.function = function http_call () {
-  
+module.exports.function = function http_call (meal) {
+  //Post 방식
   const http = require('http')
   URL = 'https://mportal.cau.ac.kr/portlet/p005/p005.ajax'
   const headers = {  
@@ -11,5 +11,20 @@ module.exports.function = function http_call () {
   const params = { "tabs":"1","tabs2":"10","daily":1 };   
   const result = http.postUrl(URL, params, headers);  
   
+  /*tabs2 = 10(조식)
+20(중식)
+40(석식)
+daily = 0(오늘)
+1(내일)*/
+
+
+//Get 방식
+/*
+  var http = require('http')
+  var result = http.getUrl("www.example.com/api",{format : 'json'})
+
+  return result
+} */
+
   return result
 }
